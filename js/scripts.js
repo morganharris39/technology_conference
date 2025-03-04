bf319d3c3950864e8655fe188bc0a5b4e05207b8
Morgan <ravenstar03@gmail.com>
2025-03-04 14:20:42 -0700

    added color and fonts

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
+// inject year in footer
+const rightNow = new Date();
+document.querySelector('#year').textContent = rightNow.getFullYear()
+
 const btnElement = document.querySelector("#hamburgerBtn")
 const navElement = document.querySelector("#primaryNav")
 
